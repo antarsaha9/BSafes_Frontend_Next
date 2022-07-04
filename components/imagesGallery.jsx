634ac251245162d2376a130404f0cc12f7b9a4a7
Shih-Chin Yang <syang@bsafes.com>
2022-07-04 17:21:10 +0800

    Testing preS3Upload API

--- components/imagesGallery.jsx
+++ components/imagesGallery.jsx
@@ -7,15 +7,79 @@ import DropdownButton from 'react-bootstrap/DropdownButton'
 import Dropdown from 'react-bootstrap/Dropdown'
 import ProgressBar from 'react-bootstrap/ProgressBar'
 
+import { PostCall } from '../lib/helper'
+
 import BSafesStyle from '../styles/BSafes.module.css'
 
 export default function ImagesGallery() {
     const fileInputRef = useRef(null);
 
-    const handleUpload = () => {
+    const handleUpload = async () => {
+        
+        try {
+            let data = await PostCall({
+                api: 'preS3Upload', 
+                body:{}
+            });
+            if(data.status == 'ok') {
+                console.log(data);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+      console.log("handleUpload")
       fileInputRef.current?.click();
     };
 
+    return (
+        <div className="images">
+            <input ref={fileInputRef} type="file" multiple className="d-none editControl" id="image" />
+            <Row className="imageBtnRowTemplate row hidden">
+                <Col sm={{span:10, offset:1}} md={{span:8, offset:2}} className="text-center">
+                    <Button onClick={handleUpload} variant="link" className="btn btn-labeled">
+                        <h4><i className="fa fa-picture-o fa-lg" aria-hidden="true"></i></h4>              
+                    </Button>
+                </Col>
+            </Row>	
+            <Row className="uploadImageTemplate downloadImageTemplate hidden">
+                <Col sm={{span:10, offset:1}} md={{span:8, offset:2}}>
+                    <div className="uploadText downloadText">
+          
+                    </div>
+                    <ProgressBar className="marginTop20Px marginBottom0Px"  now={45} />
+                </Col>
+            </Row>
+            <Row className="imagePanelTemplate hidden">
+                <Col sm={{span:10, offset:1}} md={{span:8, offset:2}}>
+                <div className="image">
+            
+                </div>
+                </Col>
+                
+                <div sm={{span:10, offset:1}} md={{span:8, offset:2}}>
+                    <DropdownButton variant="link" align="end" title={
+                        <span>
+                            <i className="fa fa-ellipsis-v" aria-hidden="true"></i>
+                         </span>
+                    }  className="pull-right" id="dropdown-menu-align-end">
+                        <Dropdown.Item eventKey="1" className="changeImageBtn">Change Image</Dropdown.Item>
+                        <Dropdown.Item eventKey="2" className="deleteImageBtn">Delete Image</Dropdown.Item>
+                    </DropdownButton>
+                    <input ref={fileInputRef} type="file" multiple className={"d-none insertImages"} />
+                    <Button onClick={handleUpload} variant="link" className="btn btn-labeled pull-right">
+                        <i className="fa fa-picture-o fa-lg" aria-hidden="true"></i>    
+                    </Button>
+                    <Button variant="link" className="btnWrite btnWriteImageWords pull-right"><i className="fa fa-pencil" aria-hidden="true"></i></Button>
+                </div>
+                <div className="col-xs-12 col-xs-offset-0 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
+                <div className="froala-editor imageWordsEditor">
+                    <p></p>
+                </div>
+                </div>
+            </Row>		
+        </div>    
+    )   
+/*
     return (
         <div className="images">
             <Row className="imageBtnRowTemplate row hidden">
@@ -64,4 +128,5 @@ export default function ImagesGallery() {
             </Row>		
         </div>    
     )
+*/
 }
