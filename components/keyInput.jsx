e492e3735722dc669765e37befa96717f8ca8d36
Shih-Chin Yang <syang@bsafes.com>
2022-07-12 15:15:32 +0800

    Working on keyInput Component

--- components/keyInput.jsx
+++ components/keyInput.jsx
@@ -1,39 +1,104 @@
 import { legacy_createStore } from '@reduxjs/toolkit';
-import { useState } from 'react'
-import { Form } from 'react-bootstrap'
+import { useEffect, useRef, useState } from 'react'
+import { Button, Form, InputGroup } from 'react-bootstrap'
+
+var masterKey = "";
+var maskedKey = "";
 
 export default function KeyInput() {
-    let keyTimer = null;
-    let key = "";
+    const inputRef = useRef(null);
+    let inputTimer = null;
+
+    const [hidden, setHidden] = useState(true);
+
+    const handleKeyDown = e => {
+        //e.preventDefault();
+
+        console.log('keyDown selection', e.target.selectionStart + ', ' +  e.target.selectionEnd);
+
+    }
 
     const handleInput = e => {
-        
+        e.preventDefault();
+
+       // console.log('selection', e.target.selectionStart + ', ' +  e.target.selectionEnd);
+        console.log("handleInput");
         console.log(e.target.value);
         const originalInput = e.target.value;
         let inputLength = originalInput.length;
-        if(originalInput.length < key.length){
-            key = key.substring(0, inputLength);
+
+        if(!hidden) {
+            masterKey = originalInput;
         } else {
-            key = key + originalInput.charAt(inputLength - 1);
+            /* Assuming the change only happens at the end of masterKey */
+            if(originalInput.length < masterKey.length){
+                masterKey = masterKey.substring(0, inputLength);
+            } else {
+                masterKey = masterKey + originalInput.charAt(inputLength -1);
+            }
         }
-        console.log("Key: ", key)
+        
+        maskedKey="";
+        for(var i=0; i< inputLength; i++) maskedKey += "*";
 
-        if(keyTimer) {
-            clearTimeout(keyTimer);
-            keyTimer = null;
+        console.log("Master Key: ", masterKey);
+        if(inputTimer) {
+            clearTimeout(inputTimer);
+            inputTimer = null;
             let maskedInput="";
-            for(var i=0; i< inputLength-1; i++) maskedInput += "*";
-            e.target.value = maskedInput + originalInput.charAt(inputLength-1);
+            if(hidden) {
+                for(var i=0; i< inputLength-1; i++) maskedInput += "*";
+                e.target.value = maskedInput + originalInput.charAt(inputLength-1);
+            } else {
+                e.target.value = masterKey;
+            }
         }
 
-        keyTimer = setTimeout(() => {
-            let maskedInput="";
-            for(var i=0; i< inputLength; i++) maskedInput += "*";
-            e.target.value = maskedInput;
+        inputTimer = setTimeout(() => {         
+            if(hidden){
+                e.target.value = maskedKey;
+            } else {
+                e.target.value = masterKey;
+            }
         }, 1000)
     }
 
+    const handlePaste = (e) => {
+        
+        e.preventDefault();
+        
+        console.log('selection', e.target.selectionStart + ', ' +  e.target.selectionEnd);
+        
+        let paste = (e.clipboardData || window.clipboardData).getData('text');
+
+        
+    }
+
+    const handleClick = e => {
+        e.preventDefault();
+
+        setHidden(!hidden);
+    }
+   
+    useEffect(() => {
+        if(hidden) {
+            inputRef.current.value = maskedKey;
+        } else {
+            inputRef.current.value = masterKey;
+        }
+    }, [hidden])
+
     return (
-        <Form.Control type="text" onInput={handleInput}></Form.Control>
+        <>
+            <InputGroup>
+                <Form.Control ref={inputRef} type="text" 
+                    onInput={handleInput}
+                    onPaste={handlePaste}
+                    onKeyDown={handleKeyDown}>
+                </Form.Control>
+                <Button onClick={handleClick} variant="outline-dark">{hidden?<i id="1" className="fa fa-eye-slash fa-lg" aria-hidden="true"></i>:<i id="1" className="fa fa-eye fa-lg" aria-hidden="true"></i>}</Button>
+            </InputGroup>
+            
+        </>
     )
 }
