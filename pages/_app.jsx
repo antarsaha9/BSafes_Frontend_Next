d53f461fcbe0593169175d7825fcbdbe63e6df1e
Shih-Chin Yang <syang@bsafes.com>
2023-06-09 14:05:00 +0800

    Working on resuming a stream

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -9,7 +9,7 @@ import { useEffect } from 'react';
 function MyApp({ Component, pageProps }) {
   useEffect(()=> {
     if("serviceWorker" in navigator) {
-      navigator.serviceWorker.register("/serviceWorkerV156.js?v173", {
+      navigator.serviceWorker.register("/serviceWorkerV156.js?v181", {
         scope: "/",
       }).then(
           function(registration) {
